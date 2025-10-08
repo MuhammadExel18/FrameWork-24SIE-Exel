7319a40826532b0bf8bb5ef0cfef79431d4e3419
Muhammad Exel <exel24si@pcr.ac.id>
2025-10-08 11:09:32 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MahasiswaController;
 Route::get('/', function () {
     return view('welcome');
 })->name('mahasiswa.show');
@@ -26,6 +27,8 @@ Route::get('/home', [HomeController::class, 'index'])-> name ('home');
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 
+Route::get('dashboard', [DashboardController::class, 'index'])
+         ->name('dashboard');
 
 
 
